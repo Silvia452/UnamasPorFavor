6a1fa8cea5c365fd9567301bcc63771eca917ef6
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-03-20 13:23:56 +0100

    Ejercicio 1.b

--- src/main/java/ejercicio1/algoritmoImplementado.java
+++ src/main/java/ejercicio1/algoritmoImplementado.java
@@ -1,4 +1,24 @@
 package ejercicio1;
 
 public class algoritmoImplementado {
+
+    static final double N = 2;
+
+    public static double bisectIter(double min, double max, double prec) {
+        double med = (min + max) / 2;
+        while (max - min >= prec) {
+            med = (min + max) / 2;
+            if (f(min) * f(med) < 0) {
+                max = med;
+            } else {
+                min = med;
+            }
+        }
+        return med;
+    }
+
+    static double f(double x) {
+        return x*x - N;
+    }
+
 }
