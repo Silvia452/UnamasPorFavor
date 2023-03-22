3fb99728acd508e2c3a43f8c1c3699f1b0029a9f
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-03-22 09:51:44 +0100

    Ejercicio 2.b y creados main

--- src/main/java/ejercicio2/Main.java
+++ src/main/java/ejercicio2/Main.java
@@ -2,6 +2,9 @@ package ejercicio2;
 
 public class Main {
     public static void main(String[] args) {
-        algoritmoImplementado2.fIter(100, 2);
+        algoritmoImplementado2<Integer, Integer> a = new algoritmoImplementado2<>();
+        a.factorizarIterativo(100);
+
+
     }
 }
--- src/main/java/ejercicio2/algoritmoImplementado2.java
+++ src/main/java/ejercicio2/algoritmoImplementado2.java
@@ -1,8 +1,12 @@
 package ejercicio2;
 
-public class algoritmoImplementado2 {
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
 
-    public static void fIter(int num, int div) {
+public class algoritmoImplementado2 <I extends Number, I1 extends Number> {
+
+    void factorizarIterativo(int num) {
+        int div = 2;
         while (num > 1) {
             if (num % div == 0) {
                 System.out.println(div);
@@ -12,5 +16,7 @@ public class algoritmoImplementado2 {
             }
         }
     }
+
+
 
 }
