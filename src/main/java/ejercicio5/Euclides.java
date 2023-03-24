958f8771d2aba24efaf200b0f61cd2c019cfda7d
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-03-24 23:36:59 +0100

    Terminado ejercicio 5

--- src/main/java/ejercicio5/Euclides.java
+++ src/main/java/ejercicio5/Euclides.java
@@ -1,4 +1,30 @@
 package ejercicio5;
 
+import java.util.function.BiFunction;
+
 public class Euclides {
+    public static int gcdRecursive(int m, int n) {
+        if (n == 0) {
+            return m;
+        } else {
+            return gcdRecursive(n, m % n);
+        }
+    }
+
+    // Implementación iterativa del algoritmo de Euclides
+    public static int gcdIterative(int m, int n) {
+        int r = m % n;
+        while (r != 0) {
+            m = n;
+            n = r;
+            r = m % n;
+        }
+        return n;
+    }
+
+    public static BiFunction<Integer, Integer, Integer> gcdLambda =
+            (m, n) -> n == 0 ? m : Euclides.gcdLambda.apply(n, m % n);
+
+
+
 }
--- src/main/java/ejercicio5/Main.java
+++ src/main/java/ejercicio5/Main.java
@@ -3,6 +3,17 @@ package ejercicio5;
 public class Main {
 
     public static void main(String[] args) {
+        int m = 24;
+        int n = 16;
+
+        int gcd1 = Euclides.gcdRecursive(m, n);
+        int gcd2 = Euclides.gcdIterative(m, n);
+        int gcd3 = Euclides.gcdLambda.apply(m, n);
+
+        System.out.println("El MCD de " + m + " y " + n + " es:");
+        System.out.println("- " + gcd1 + " (recursivo)");
+        System.out.println("- " + gcd2 + " (iterativo)");
+        System.out.println("- " + gcd3 + " (expresión lambda)");
 
     }
 }
