02b7a1b0cb3b1e53464a9e2dfba6a21e60cda3bd
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-03-24 23:39:55 +0100

    Terminado

--- src/main/java/ejercicio4/Main.java
+++ src/main/java/ejercicio4/Main.java
@@ -7,8 +7,21 @@ public class Main {
         Scanner sn = new Scanner(System.in);
         System.out.println("Ingrese una cadena: ");
         String cadena = sn.nextLine();
-        System.out.println(palindromo.esPalindromoRecursivo(cadena));
-        System.out.println(palindromo.esPalindromoIterativo(cadena));
+        if (cadena.length() == 0) {
+            System.out.println("La cadena no puede estar vacia");
+            return;
+        }
+        if (palindromo.esPalindromoIterativo(cadena)) {
+            System.out.println("La cadena es palindromo de forma iterativa");
+        } else {
+            System.out.println("La cadena no es palindromo");
+        }
+        if (palindromo.esPalindromoRecursivo(cadena)) {
+            System.out.println("La cadena es palindromo de forma recursiva");
+        } else {
+            System.out.println("La cadena no es palindromo");
+        }
+
 
     }
 }
